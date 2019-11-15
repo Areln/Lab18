3b7aed4fdda5ce99981ac7c49647e1b28c61fc4c
Aaron <devilfruits1999@gmail.com>
2019-11-15 13:25:34 -0500

    Initial Commit

---
+++ Lab18 WebApp/Controllers/RegistrationController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lab18_WebApp.Models;
+
+namespace Lab18_WebApp.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult UserSummary(RegisterUser newUser) 
+        {
+            if (ModelState.IsValid)
+            {
+                return View(newUser);
+            }
+            else
+            {
+                return View("Index");
+            }
+
+        }
+    }
+}
---
+++ Lab18 WebApp/Models/RegisterUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab18_WebApp.Models
+{
+    public class RegisterUser
+    {
+        [Required]
+        [MaxLength(25, ErrorMessage = "Your user name length must be between 3 and 25 characters"), MinLength(3, ErrorMessage = "Your user name length must be between 3 and 25 characters")]
+        public string UserName { get; set; }
+        [Required]
+        public string UserEmail { get; set; }
+        [Required, MaxLength(20, ErrorMessage = "Password must contain atleast 5 letters and 1 Number"), MinLength(6, ErrorMessage = "Password must contain atleast 5 letters and 1 Number"), RegularExpression(@".*[0-9].*", ErrorMessage = "Password must contain atleast 5 letters and 1 Number")]
+        public string UserPass { get; set; }
+
+    }
+}
